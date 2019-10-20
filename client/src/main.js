1c5411d817e70aa66183a7736bd728ac2a7b0a2e
Shane Walsh <walsh.shane@icloud.com>
2019-10-20 17:51:36 +0100

    Refactor errorLink

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,11 @@
+/* eslint-disable */ 
 import Vue from 'vue'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-//import { onError } from "apollo-link-error"
+import { onError } from "apollo-link-error"
 import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-//import { enableExperimentalFragmentVariables } from 'graphql-tag'
+import { enableExperimentalFragmentVariables } from 'graphql-tag'
 import VueApollo from 'vue-apollo'
 import ElementUI from 'element-ui'
 
@@ -24,19 +25,19 @@ const httpLink = new HttpLink({uri})
 
 const cache = new InMemoryCache({})
 
-/*const errorLink = onError(({ graphQLErrors, networkError }) => {
+const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.map(({ message, locations, path }) =>
       console.log(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     )
-  if (networkError) console.log(`[Network error]: ${networkError}`)
-})*/
+  if (networkError) window.console.log(`[Network error]: ${networkError}`)
+})
 
 const client = new ApolloClient({
   link: ApolloLink.from([
-    //errorLink,
+    errorLink,
     httpLink
   ]),
   cache,
